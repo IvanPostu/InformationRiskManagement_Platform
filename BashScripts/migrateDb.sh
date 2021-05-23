f0a06140947e9905ad00ebb4e2fd82b6b23b197b
IvanPostu <ipostu20000127@gmail.com>
2021-05-23 09:47:06 +0300

    Bash script for db migration.

---
+++ BashScripts/migrateDb.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+SCRIPT=`realpath -s $0`
+SCRIPTPATH=`dirname $SCRIPT`
+ROOT_PROJECT_DIR="$SCRIPTPATH/.."
+
+if [ "$1" = "" ]
+then 
+    exit -1
+fi
+
+$ROOT_PROJECT_DIR//mvnw -f ./irme-db-migration/pom.xml liquibase:$1 "-Dprofile=dev"
