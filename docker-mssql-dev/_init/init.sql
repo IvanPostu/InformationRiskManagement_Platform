3e98c9836d6f8b330041acbda43c79bf3f4c5842
IvanPostu <ipostu20000127@gmail.com>
2021-05-23 20:47:41 +0300

    Docker SQL Server - init script updated.

--- docker-mssql-dev/_init/init.sql
+++ docker-mssql-dev/_init/init.sql
@@ -1,8 +1,8 @@
 USE [master]
 
----------------------------------------------------------------------------
--- Create Database - InformationRiskManagementDatabase  -------------------
----------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- Create Database - InformationRiskManagementDatabase  ------------------------
+--------------------------------------------------------------------------------
 CREATE DATABASE [InformationRiskManagementDatabase] ON  PRIMARY 
 (	NAME = N'InformationRiskManagementDatabase', 
 	FILENAME = N'/home/mssql/database/InformationRiskManagementDatabase_Data.mdf' , 
@@ -11,7 +11,22 @@ CREATE DATABASE [InformationRiskManagementDatabase] ON  PRIMARY
 LOG ON 
 ( 	NAME = N'InformationRiskManagementDatabase_Log', 
 	FILENAME = N'/home/mssql/database/InformationRiskManagementDatabase_Log.ldf' , 
-	SIZE = 2048KB , MAXSIZE = 2048GB , FILEGROWTH = 16384KB 
+	SIZE = 2048KB , MAXSIZE = 4GB , FILEGROWTH = 16384KB 
+)
+GO
+
+--------------------------------------------------------------------------------
+-- Create Database - InformationRiskManagementDatabase for integration tests ---
+--------------------------------------------------------------------------------
+CREATE DATABASE [InformationRiskManagementDatabase_Test] ON  PRIMARY 
+(	NAME = N'InformationRiskManagementDatabase_Test', 
+	FILENAME = N'/home/mssql/database/InformationRiskManagementDatabase_Test_Data.mdf' , 
+	SIZE = 167872KB , MAXSIZE = UNLIMITED, FILEGROWTH = 16384KB 
+)
+LOG ON 
+( 	NAME = N'InformationRiskManagementDatabase_Test_Log', 
+	FILENAME = N'/home/mssql/database/InformationRiskManagementDatabase_Test_Log.ldf' , 
+	SIZE = 2048KB , MAXSIZE = 4GB , FILEGROWTH = 16384KB 
 )
 GO
 
@@ -22,26 +37,40 @@ CREATE LOGIN ApplicationUserLogin WITH PASSWORD = 'qwYertyZ_654321'
 GO
 CREATE LOGIN MigrationUserLogin WITH PASSWORD = 'qwYertyZ_654321' 
 GO
-USE [InformationRiskManagementDatabase]
 
 -- Migration user
+USE [InformationRiskManagementDatabase]
 CREATE USER jim FOR LOGIN MigrationUserLogin 
 GO
-
 GRANT ALL PRIVILEGES 
 ON DATABASE::InformationRiskManagementDatabase TO jim 
 GO
 GRANT ALTER ON SCHEMA::dbo TO [jim] 
 GO
 
--- Application user with CRUD priveleges
-CREATE USER bob FOR LOGIN ApplicationUserLogin 
+USE [InformationRiskManagementDatabase_Test]
+CREATE USER jim FOR LOGIN MigrationUserLogin 
+GO
+GRANT ALL PRIVILEGES 
+ON DATABASE::InformationRiskManagementDatabase_Test TO jim 
+GO
+GRANT ALTER ON SCHEMA::dbo TO [jim] 
 GO
 
+-- Application user with CRUD priveleges
+USE [InformationRiskManagementDatabase]
+CREATE USER bob FOR LOGIN ApplicationUserLogin 
+GO
 GRANT INSERT, UPDATE, DELETE, SELECT 
 ON DATABASE :: InformationRiskManagementDatabase TO bob 
 GO
 
+USE [InformationRiskManagementDatabase]
+CREATE USER bob FOR LOGIN ApplicationUserLogin 
+GO
+GRANT INSERT, UPDATE, DELETE, SELECT 
+ON DATABASE :: InformationRiskManagementDatabase_Test TO bob 
+GO
 
 
 
