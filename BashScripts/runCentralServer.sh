7579a72791a2695c3a70543773d25fefa55e7447
IvanPostu <ipostu20000127@gmail.com>
2021-06-17 22:47:41 +0300

    Run webapp script - suspend turn OFF

--- BashScripts/runCentralServer.sh
+++ BashScripts/runCentralServer.sh
@@ -6,17 +6,8 @@ SCRIPT=`realpath -s $0`
 SCRIPTPATH=`dirname $SCRIPT`
 ROOT_PROJECT_DIR="$SCRIPTPATH/.."
 
-ATTACH_DEBUGGER_FLAG='-a'
 
-if [ "$1" = "$ATTACH_DEBUGGER_FLAG" ]
-then 
-    $ROOT_PROJECT_DIR/mvnw -f ./irme-central-server/pom.xml spring-boot:run \
-        -Dspring-boot.run.jvmArguments="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005 -Dspring.profiles.active=$ACTIVE_PROFILE"
-else
-    echo "If you want to debug remotely add flag $ATTACH_DEBUGGER_FLAG"
-
-    $ROOT_PROJECT_DIR/mvnw -f ./irme-central-server/pom.xml spring-boot:run \
-        -Dspring-boot.run.jvmArguments="-Dspring.profiles.active=$ACTIVE_PROFILE"
-fi
+$ROOT_PROJECT_DIR/mvnw -f ./irme-central-server/pom.xml spring-boot:run \
+    -Dspring-boot.run.jvmArguments="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Dspring.profiles.active=$ACTIVE_PROFILE"
 
 
