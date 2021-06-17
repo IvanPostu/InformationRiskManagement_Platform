8588dde5c7ec822e6b35a0a897914ce64d6c9efb
IvanPostu <ipostu20000127@gmail.com>
2021-06-17 21:37:12 +0300

    Improve development scripts

--- BashScripts/runAdminServer.sh
+++ BashScripts/runAdminServer.sh
@@ -6,7 +6,8 @@ SCRIPT=`realpath -s $0`
 SCRIPTPATH=`dirname $SCRIPT`
 ROOT_PROJECT_DIR="$SCRIPTPATH/.."
 
+export MAVEN_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=4000,server=y,suspend=n"
 
 $ROOT_PROJECT_DIR/mvnw -f ./irme-central-server/irme-admin-mvc/pom.xml \
-    jetty:run
+    jetty:run 
 
