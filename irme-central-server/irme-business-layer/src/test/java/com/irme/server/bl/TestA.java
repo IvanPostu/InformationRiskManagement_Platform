e110b3aa3a59238b7321f3124b40fe010246ac25
IvanPostu <ipostu20000127@gmail.com>
2021-07-04 22:05:59 +0300

    Fix problem with logback on Windows

---
+++ irme-central-server/irme-business-layer/src/test/java/com/irme/server/bl/TestA.java
@@ -0,0 +1,16 @@
+package com.irme.server.bl;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestA {
+    private final Logger logger = LoggerFactory.getLogger(TestA.class);
+
+    @Test
+    public void aTest() {
+        logger.info("Hello");
+        Assertions.assertEquals(22, 22);
+    }
+}
--- irme-central-server/irme-common/src/test/java/com/irme/common/AppTest.java
+++ irme-central-server/irme-common/src/test/java/com/irme/common/AppTest.java
@@ -1,20 +1,20 @@
-// package com.irme.common;
+package com.irme.common;
 
-// import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-// import org.junit.Test;
 
-// /**
-//  * Unit test for simple App.
-//  */
-// public class AppTest
-// {
-//     /**
-//      * Rigorous Test :-)
-//      */
-//     @Test
-//     public void shouldAnswerWithTrue()
-//     {
-//         assertTrue( true );
-//     }
-// }
+
+public class AppTest {
+    private final Logger logger = LoggerFactory.getLogger(AppTest.class);
+
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        logger.info("Hello");
+
+        Assertions.assertTrue(true);
+    }
+}
