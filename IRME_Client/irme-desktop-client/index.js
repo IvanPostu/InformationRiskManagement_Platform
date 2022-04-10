2e9a8e56a672dacb341f493a243fb859c21a4dba
IvanPostu <ipostu20000127@gmail.com>
2022-04-10 14:55:37 +0300

    Create electron project

---
+++ IRME_Client/irme-desktop-client/index.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron')
+
+let isDev = false
+let mainWindow
+
+const createWindow = () => {
+    isDev = process.argv.findIndex((a) => a === '--development-mode') !== -1;
+    const runningMode = isDev ? 'development' : 'production';
+
+    console.log(`Application is running in ${runningMode} mode.`);
+
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true,
+            webSecurity: false,
+            contextIsolation: false,
+        },
+    });
+
+    mainWindow.loadURL('https://github.com');
+    // if (isDev) {
+    //     mainWindow.loadURL('http://127.0.0.1:8000');
+    // } else {
+    //     mainWindow.loadFile('./build/app/index.html');
+    // }
+};
+
+app.on('ready', createWindow);
