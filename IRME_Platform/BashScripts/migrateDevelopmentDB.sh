844b28e172123f3087ad34e36daa70e4b36bbe15
IvanPostu <ipostu20000127@gmail.com>
2022-04-16 18:27:29 +0300

    Evaluation table change

--- IRME_Platform/BashScripts/migrateDevelopmentDB.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ACTIVE_PROFILE="dev"
-
-SCRIPT=`realpath -s $0`
-SCRIPTPATH=`dirname $SCRIPT`
-ROOT_PROJECT_DIR="$SCRIPTPATH/.."
-
-$ROOT_PROJECT_DIR/mvnw -f ./irme-db-migration/pom.xml -Denv=development process-resources liquibase:updateSQL
--- IRME_Platform/irme-db-migration/src/main/resources/db/changelog/common/tables/sa__processes.sql
+++ IRME_Platform/irme-db-migration/src/main/resources/db/changelog/common/tables/sa__processes.sql
@@ -1,8 +1,13 @@
 
 CREATE TABLE [dbo].[sa__processes] (
     [process_id]                    INTEGER IDENTITY(1, 1) NOT NULL,
-    [organisation_id]			          INTEGER NOT NULL,
+    [organisation_id]			    INTEGER NOT NULL,
     [created]                       DATE,
+    [author_user_id]                INTEGER,
+
+    --0:open, 1:completed, 2:force_closed
+    [status]                        INTEGER DEFAULT 0,
+    [category_id]                   INTEGER,
 
     CONSTRAINT pk_sa__processes PRIMARY KEY CLUSTERED ([process_id] ASC)
 );
