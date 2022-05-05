09ab464cef5397a40979717e07a6b1c408d338a5
IvanPostu <ipostu20000127@gmail.com>
2022-05-05 19:53:49 +0300

    Change manual scripts & add qa for all categories

---
+++ IRME_Platform/irme-db-migration/src/main/resources/4manual23456789_10_2.sql
@@ -0,0 +1,13 @@
+
+
+execute dev__insert_test_questions_answers N'Verificarea autenticității'
+execute dev__insert_test_questions_answers N'Management și monitoring'
+execute dev__insert_test_questions_answers N'Mediu de hostare a aplicațiilor web'
+execute dev__insert_test_questions_answers N'Politici de securitate'
+execute dev__insert_test_questions_answers N'Backup și recuperare'
+execute dev__insert_test_questions_answers N'Plasarea și utilizarea serviciilor'
+execute dev__insert_test_questions_answers N'Stocarea datelor și comunicații'
+execute dev__insert_test_questions_answers N'Cerințe și evaluări'
+execute dev__insert_test_questions_answers N'Politici și proceduri'
+execute dev__insert_test_questions_answers N'Formare și conștientizare (resurse umane)'
+
---
+++ IRME_Platform/irme-db-migration/src/main/resources/6manual_4.sql
@@ -0,0 +1,26 @@
+
+BEGIN
+
+    DECLARE @loopCounter INTEGER = 1; --11
+
+
+    WHILE @loopCounter <= 11
+    BEGIN
+      SET @d = DATEADD(DAY, -1, GETDATE());
+      EXECUTE [dbo].[dev__evaluate_organisation] 4, @loopCounter, 9, @d;
+      SET @d = DATEADD(DAY, -2, GETDATE());
+      EXECUTE [dbo].[dev__evaluate_organisation] 4, @loopCounter, 9, @d;
+      SET @d = DATEADD(DAY, -3, GETDATE());
+      EXECUTE [dbo].[dev__evaluate_organisation] 4, @loopCounter, 9, @d;
+      SET @d = DATEADD(DAY, -4, GETDATE());
+      EXECUTE [dbo].[dev__evaluate_organisation] 4, @loopCounter, 9, @d;
+      SET @d = DATEADD(DAY, -5, GETDATE());
+      EXECUTE [dbo].[dev__evaluate_organisation] 4, @loopCounter, 9, @d;
+      SET @d = DATEADD(DAY, -6, GETDATE());
+      EXECUTE [dbo].[dev__evaluate_organisation] 4, @loopCounter, 9, @d;
+
+      SET @loopCounter = @loopCounter + 1;
+    END
+
+END
+
